fb54f8806e323c88e23148aca58a1a46be8f1c65
Victor J. Bezak III <vbezak@razoyo.com>
2020-02-05 13:27:07 -0600

    Cleaned up logic

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,35 @@
 module Main exposing (main)
 
 import Array exposing (..)
--- import Browser
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
--- import PlaySpace
+import PlaySpace
 
 
 -- MODEL
 
-{--
+{-
+    We could add a Tuple for type Player, where we hold both players.
+
+    type alias Player =
+        { username: String
+        , winRecord: Tuple Int
+        , level: Int
+        }
+
+    OR
+
+    type PlayerAction
+        = Won Player
+        | Lost Player
+        | Draw
+-}
 type alias Model =
     { cells : Array Int
-    , activePlayer : Int
-    , winner : Int
+    , activePlayerId : Int
+    , winnerId : Int
     , draw : Bool
     , description : String
     }
@@ -22,83 +37,36 @@ type alias Model =
 model : Model
 model =
     { cells = PlaySpace.cells
-    , activePlayer = 1
-    , winner = 0
+    , activePlayerId = 1
+    , winnerId = 0
     , draw = False
     , description = ""
     }
---}
+
 
 -- VIEW
 
-{--
-view : Model -> Html msg
+view : Model -> Html Msg
 view game = 
---}
-    {--
     div [ class "playspace" ]
-        ( Array.map renderCell game.cells )
+        (toList (Array.map renderCell game.cells))
 
-    renderCell : Int -> Html Msg
-    renderCell content =
-        if content == 1 then
-            div [ class "O-cell" ] [ text "O" ]
+renderCell : Int -> Html Msg
+renderCell content =
+    if content == 1 then
+        div [ class "O-cell" ] [ text "O" ]
 
-        else if content == 2 then
-            div [ class "X-cell" ] [ text "X" ]
+    else if content == 2 then
+        div [ class "X-cell" ] [ text "X" ]
 
-        else
-            div [ class "empty-cell"
-                , onClick CellClicked
-                ] []
-    --}
-
-    {--
-    div [ class "playspace" ]
-        [ div [ class "cell", id "A1", onClick CellClicked ] []
-        , div [ class "cell", id "A2", onClick CellClicked ] []
-        , div [ class "cell", id "A3", onClick CellClicked ] []
-        , div [ class "cell", id "B1", onClick CellClicked ] []
-        , div [ class "cell", id "B2", onClick CellClicked ] []
-        , div [ class "cell", id "B3", onClick CellClicked ] []
-        , div [ class "cell", id "C1", onClick CellClicked ] []
-        , div [ class "cell", id "C2", onClick CellClicked ] []
-        , div [ class "cell", id "C3", onClick CellClicked ] []
-        ]
-    --}
-
-layout : Html msg
-layout =
-    div [ id "container" ]
-        [ header [ id "title" ] [ text "Tic-Tac-Toe" ]
-        , main_ [ id "playspace" ]
-            [ button [ id "A1" ] []
-            , button [ id "A2" ] []
-            , button [ id "A3" ] []
-            , button [ id "B1" ] []
-            , button [ id "B2" ] []
-            , button [ id "B3" ] []
-            , button [ id "C1" ] []
-            , button [ id "C2" ] []
-            , button [ id "C3" ] []
-            ]
-        , footer [ id "footer" ]
-            [ div [ id "player1" ]
-                [ p [ class "playerName" ] [ text "Player 1"]
-                , p [ class "playerWinCount" ] [ text "Win Count: "]
-                ]
-            , div [ id "player2" ]
-                [ p [ class "playerName" ] [ text "Player 2"]
-                , p [ class "playerWinCount" ] [ text "Win Count: "]
-                ]
-            ]
-        ]
-        
+    else
+        div [ class "empty-cell"
+            , onClick (CellClicked "click") --WE NEED TO PASS THE ID INT OF THE CELL THAT WAS CLICKED
+            ] []     
     
 
 -- UPDATE
 
-{--
 type Msg
     = CellClicked String
     | GameOver String
@@ -109,25 +77,27 @@ update msg game =
     case msg of
         CellClicked desc ->
             { game | description = desc
-            , activePlayer = ( modBy 2 game.activePlayer ) + 1
+            , activePlayerId = ( modBy 2 game.activePlayerId ) + 1
             }
 
         GameOver conclusion ->
             if conclusion == "draw" then
-                { game | draw = True }
+                { game | draw = True
+                , description = conclusion
+                }
             
             else
-                { game | winner = game.activePlayer }
---}
-            
+                let
+                    winner = game.activePlayerId
+                
+                in
+                { game | winnerId = winner
+                , description = "Player " ++ String.fromInt winnerId ++ " Won the Game!"
+                }   
 
 
-{--}
-main : Html msg
-main = layout
---}
+-- INITIALIZE
 
-{--
 main : Program () Model Msg
 main =
     Browser.sandbox
@@ -135,4 +105,3 @@ main =
         , view = view
         , update = update
         }
---}
