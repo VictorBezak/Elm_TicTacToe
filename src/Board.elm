6afc7d72e87239977e5346cde94e6735bf645297
Victor J. Bezak III <vbezak@razoyo.com>
2020-02-07 11:39:52 -0600

    update board model

--- src/Board.elm
+++ src/Board.elm
@@ -38,29 +38,29 @@ type Cell
     | C3
 
 type alias Board =
-    { a1 : Cell { content : Content, clickable : Clickable }
-    , a2 : Cell { content : Content, clickable : Clickable }
-    , a3 : Cell { content : Content, clickable : Clickable }
-    , b1 : Cell { content : Content, clickable : Clickable }
-    , b2 : Cell { content : Content, clickable : Clickable }
-    , b3 : Cell { content : Content, clickable : Clickable }
-    , c1 : Cell { content : Content, clickable : Clickable }
-    , c2 : Cell { content : Content, clickable : Clickable }
-    , c3 : Cell { content : Content, clickable : Clickable }
+    { a1 : { id : Cell, content : Content, clickable : Clickable }
+    , a2 : { id : Cell, content : Content, clickable : Clickable }
+    , a3 : { id : Cell, content : Content, clickable : Clickable }
+    , b1 : { id : Cell, content : Content, clickable : Clickable }
+    , b2 : { id : Cell, content : Content, clickable : Clickable }
+    , b3 : { id : Cell, content : Content, clickable : Clickable }
+    , c1 : { id : Cell, content : Content, clickable : Clickable }
+    , c2 : { id : Cell, content : Content, clickable : Clickable }
+    , c3 : { id : Cell, content : Content, clickable : Clickable }
     }
 
     
 board : Board
 board =
-    { a1 = A1 { content = Empty, clickable = Active }
-    , a2 = A2 { content = Empty, clickable = Active }
-    , a3 = A3 { content = Empty, clickable = Active }
-    , b1 = B1 { content = Empty, clickable = Active }
-    , b2 = B2 { content = Empty, clickable = Active }
-    , b3 = B3 { content = Empty, clickable = Active }
-    , c1 = C1 { content = Empty, clickable = Active }
-    , c2 = C2 { content = Empty, clickable = Active }
-    , c3 = C3 { content = Empty, clickable = Active }
+    { a1 = { id = A1, content = Empty, clickable = Active }
+    , a2 = { id = A2, content = Empty, clickable = Active }
+    , a3 = { id = A3, content = Empty, clickable = Active }
+    , b1 = { id = B1, content = Empty, clickable = Active }
+    , b2 = { id = B2, content = Empty, clickable = Active }
+    , b3 = { id = B3, content = Empty, clickable = Active }
+    , c1 = { id = C1, content = Empty, clickable = Active }
+    , c2 = { id = C2, content = Empty, clickable = Active }
+    , c3 = { id = C3, content = Empty, clickable = Active }
     }
 
 showCell : Cell -> String
